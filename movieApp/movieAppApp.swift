821cb2c406cc1b844b6d6d073df19e12322a3009
pierrehlvt <pierre.halavent@gmail.com>
2023-03-27 09:55:02 +0200

    Initial Commit

---
+++ movieApp/movieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieAppApp.swift
+//  movieApp
+//
+//  Created by Pierre Halavent on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct movieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
